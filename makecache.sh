dd77ab9f3ba4e21ebf14aaa35f646ab98f972d5d
samarth kulshrestha <samarthkulshrestha@protonmail.com>
2025-02-09 15:48:04 +0530

    added a script for cachedir generation

---
+++ makecache.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+mkdir -p ~/.local/share/encephalon/cache/source
+mkdir -p ~/.local/share/encephalon/cache/source/pdf
+mkdir -p ~/.local/share/encephalon/cache/source/txt
+mkdir -p ~/.local/share/encephalon/cache/source/epub
+mkdir -p ~/.local/share/encephalon/cache/source/trans
+mkdir -p ~/.local/share/encephalon/cache/proc
+mkdir -p ~/.local/share/encephalon/cache/proc/pdf
+mkdir -p ~/.local/share/encephalon/cache/proc/epub
+mkdir -p ~/.local/share/encephalon/cache/proc/trans
